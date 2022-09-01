476199120dda4781c876b90460006779d9e9fdee
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-09-01 17:51:11 +0200

    example: add logger + comments

--- example/main.cpp
+++ example/main.cpp
@@ -1,18 +1,25 @@
 #include <configcat/configcatclient.h>
 #include <configcat/configcatuser.h>
+#include "configcat/log.h"
 #include <iostream>
 
 using namespace std;
 using namespace configcat;
 
 int main(int /*argc*/, char** /*argv*/) {
+    // Info level logging helps to inspect the feature flag evaluation process.
+    // Use the default warning level to avoid too detailed logging in your application.
+    configcat::setLogLevel(LOG_LEVEL_INFO);
+
+    // Initialize the ConfigCatClient with an SDK Key.
     auto client = ConfigCatClient::get("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ");
+
+    // Creating a user object to identify your user (optional).
     auto user = ConfigCatUser(
         "user-id",
         "configcat@example.com",
-        "country", {
-            {"version", "1.0.0"}
-        }
+        "country",
+        { { "version", "1.0.0" } }
     );
 
     bool value = client->getValue("isPOCFeatureEnabled", false, &user);
