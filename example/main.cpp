5bf95072c453acdaf8a2e3e9e158e0e38a20c862
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-09-01 18:05:37 +0200

    example: add console logger

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,7 @@
 #include <configcat/configcatclient.h>
 #include <configcat/configcatuser.h>
 #include "configcat/log.h"
+#include "configcat/consolelogger.h"
 #include <iostream>
 
 using namespace std;
@@ -10,6 +11,8 @@ int main(int /*argc*/, char** /*argv*/) {
     // Info level logging helps to inspect the feature flag evaluation process.
     // Use the default warning level to avoid too detailed logging in your application.
     configcat::setLogLevel(LOG_LEVEL_INFO);
+    ConsoleLogger logger;
+    configcat::setLogger(&logger);
 
     // Initialize the ConfigCatClient with an SDK Key.
     auto client = ConfigCatClient::get("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ");
