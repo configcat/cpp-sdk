f136543f8009ad430a6edfe123b1bc9a972cf60a
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-09-13 01:37:13 +0200

    logUser test

--- test/test-log.cpp
+++ test/test-log.cpp
@@ -1,15 +1,18 @@
 #include <gtest/gtest.h>
 #include "configcat/log.h"
+#include "configcat/configcatuser.h"
+#include <nlohmann/json.hpp>
 
 using namespace configcat;
 using namespace std;
+using json = nlohmann::json;
 
 class LogTest : public ::testing::Test {
 public:
     class TestLogger : public ILogger {
     public:
         void log(LogLevel level, const std::string& message) override {
-            text += "["s + logLevelAsString(level) + "] "s + message + "\n";
+            text += message + "\n";
         }
         std::string text;
     };
@@ -27,14 +30,31 @@ public:
     }
 };
 
+TEST_F(LogTest, LogUser) {
+    auto user = ConfigCatUser(
+        "id",
+        "email",
+        "country",
+        {{"custom", "test"}}
+    );
+    LOG_INFO << user;
+
+    json userJson = json::parse(testLogger.text);
+
+    EXPECT_EQ("id", userJson["Identifier"]);
+    EXPECT_EQ("email", userJson["Email"]);
+    EXPECT_EQ("country", userJson["Country"]);
+    EXPECT_EQ("test",  userJson["custom"]);
+}
+
 TEST_F(LogTest, LogIntVector) {
     std::vector<int> v = { 1, 2, 3 };
     LOG_INFO << v;
-    EXPECT_EQ("[Info] [1, 2, 3]\n", testLogger.text);
+    EXPECT_EQ("[1, 2, 3]\n", testLogger.text);
 }
 
 TEST_F(LogTest, LogStringVector) {
     std::vector<string> v = { "a", "b", "c" };
     LOG_INFO << v;
-    EXPECT_EQ("[Info] [a, b, c]\n", testLogger.text);
+    EXPECT_EQ("[a, b, c]\n", testLogger.text);
 }
