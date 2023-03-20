7d50fb02d889a3638180e33cdb96276a428a62dd
Peter Adam Korodi <52385411+kp-cat@users.noreply.github.com>
2023-03-20 14:22:49 +0100

    ConsoleLogger constructor fix (#7)

--- include/configcat/consolelogger.h
+++ include/configcat/consolelogger.h
@@ -7,6 +7,8 @@ namespace configcat {
 
 class ConsoleLogger : public ILogger {
 public:
+    ConsoleLogger(LogLevel logLevel = LOG_LEVEL_WARNING): ILogger(logLevel) {}
+
     void log(LogLevel level, const std::string& message) override {
         printf("[%s]: %s\n", logLevelAsString(level), message.c_str());
     }
