1413fae309c7556ee50bbebfd120b0265eeb2cfe
kp-cat <52385411+kp-cat@users.noreply.github.com>
2024-03-28 15:38:02 +0100

    OverrideValueTypeMismatchShouldBeHandledCorrectly_SimplifiedConfig

--- test/test-override.cpp
+++ test/test-override.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "configcat/mapoverridedatasource.h"
 #include "configcat/fileoverridedatasource.h"
 #include "configcat/configcatclient.h"
@@ -8,15 +9,17 @@
 #include "test.h"
 #include <filesystem>
 #include <fstream>
+#include <nlohmann/json.hpp>
 
-
+using json = nlohmann::json;
 using namespace configcat;
 using namespace std;
 
 
-class OverrideTest : public ::testing::Test {
+template <class T>
+class OverrideTestBase : public T {
 public:
-    ~OverrideTest() {
+    void TearDown() {
         for (auto& tempFile : tempFiles) {
             remove(tempFile.c_str());
         }
@@ -48,6 +51,8 @@ public:
     shared_ptr<MockHttpSessionAdapter> mockHttpSessionAdapter = make_shared<MockHttpSessionAdapter>();
     shared_ptr<ConfigCatLogger> logger = make_shared<ConfigCatLogger>(make_shared<ConsoleLogger>(), make_shared<Hooks>());
 };
+
+class OverrideTest : public OverrideTestBase<testing::Test> {};
 
 TEST_F(OverrideTest, Map) {
     const std::unordered_map<std::string, Value>& map = {
@@ -178,6 +183,92 @@ TEST_F(OverrideTest, ReloadFile) {
     file.close();
 
     EXPECT_TRUE(client->getValue("enabledFeature", false));
+
+    ConfigCatClient::closeAll();
+}
+
+template<typename ValueType>
+void checkTypeMismatch(const ConfigCatClient& client,
+                       std::shared_ptr<TestLogger>& logger,
+                       const std::string& key,
+                       const string& overrideValueJson,
+                       const ValueType& defaultValue,
+                       SettingType defaultValueSettingType,
+                       const Value& expectedReturnValue) {
+    logger->clear();
+    EvaluationDetails<ValueType> details = client.getValueDetails(key, defaultValue);
+
+    auto overrideValue = json::parse(overrideValueJson);
+    if (overrideValue.is_boolean() && defaultValueSettingType == SettingType::Boolean ||
+        overrideValue.is_string() && defaultValueSettingType == SettingType::String ||
+        overrideValue.is_number_integer() && defaultValueSettingType == SettingType::Int ||
+        overrideValue.is_number_float() && defaultValueSettingType == SettingType::Double) {
+        EXPECT_FALSE(details.isDefaultValue);
+        EXPECT_EQ(expectedReturnValue, Value(details.value));
+        EXPECT_EQ(nullopt, details.errorMessage);
+        EXPECT_EQ(nullptr, details.errorException);
+    } else {
+        EXPECT_TRUE(details.isDefaultValue);
+        EXPECT_EQ(expectedReturnValue, Value(details.value));
+        if (overrideValue.is_boolean() || overrideValue.is_string() || overrideValue.is_number()) {
+            EXPECT_THAT(logger->text, ::testing::HasSubstr("The type of a setting must match the type of the specified default value."));
+        } else {
+            EXPECT_THAT(logger->text, ::testing::HasSubstr("Setting type is invalid."));
+        }
+    }
+}
+
+class OverrideValueTypeMismatchShouldBeHandledCorrectly_SimplifiedConfigTestSuite : public OverrideTestBase<::testing::TestWithParam<tuple<string, Value, Value>>> {};
+INSTANTIATE_TEST_SUITE_P(OverrideTest, OverrideValueTypeMismatchShouldBeHandledCorrectly_SimplifiedConfigTestSuite, ::testing::Values(
+    make_tuple("true", false, true),
+    make_tuple("true", "", ""),
+    make_tuple("true", 0, 0),
+    make_tuple("true", 0.0, 0.0),
+    make_tuple("\"text\"", false, false),
+    make_tuple("\"text\"", "", "text"),
+    make_tuple("\"text\"", 0, 0),
+    make_tuple("\"text\"", 0.0, 0.0),
+    make_tuple("42", false, false),
+    make_tuple("42", "", ""),
+    make_tuple("42", 0, 42),
+    make_tuple("42", 0.0, 0.0),
+    make_tuple("42.0", false, false),
+    make_tuple("42.0", "", ""),
+    make_tuple("42.0", 0, 0),
+    make_tuple("42.0", 0.0, 42.0),
+    make_tuple("3.14", false, false),
+    make_tuple("3.14", "", ""),
+    make_tuple("3.14", 0, 0),
+    make_tuple("3.14", 0.0, 3.14),
+    make_tuple("null", false, false),
+    make_tuple("[]", false, false),
+    make_tuple("{}", false, false)
+));
+TEST_P(OverrideValueTypeMismatchShouldBeHandledCorrectly_SimplifiedConfigTestSuite, OverrideValueTypeMismatchShouldBeHandledCorrectly_SimplifiedConfig) {
+    auto [overrideValueJson, defaultValue, expectedReturnValue] = GetParam();
+    const string key = "flag";
+
+    auto filePath = createTemporaryFile(string_format(R"({ "flags": { "%s": %s } })", key.c_str(), overrideValueJson.c_str()));
+
+    std::shared_ptr<TestLogger> testLogger = make_shared<TestLogger>(LOG_LEVEL_WARNING);
+    ConfigCatOptions options;
+    options.pollingMode = PollingMode::manualPoll();
+    options.logger = testLogger;
+    options.flagOverrides = make_shared<FileFlagOverrides>(filePath, LocalOnly);
+    auto client = ConfigCatClient::get(kTestSdkKey, &options);
+
+    auto defaultValueSettingType = static_cast<SettingValue>(Value(defaultValue)).getSettingType();
+    if (holds_alternative<bool>(defaultValue)) {
+        checkTypeMismatch(*client, testLogger, key, overrideValueJson, get<bool>(defaultValue), defaultValueSettingType, expectedReturnValue);
+    } else if (holds_alternative<string>(defaultValue)) {
+        checkTypeMismatch(*client, testLogger, key, overrideValueJson, get<string>(defaultValue), defaultValueSettingType, expectedReturnValue);
+    } else if (holds_alternative<int>(defaultValue)) {
+        checkTypeMismatch(*client, testLogger, key, overrideValueJson, get<int>(defaultValue), defaultValueSettingType, expectedReturnValue);
+    } else if (holds_alternative<double>(defaultValue)) {
+        checkTypeMismatch(*client, testLogger, key, overrideValueJson, get<double>(defaultValue), defaultValueSettingType, expectedReturnValue);
+    } else {
+        throw std::runtime_error("Return value type is invalid.");
+    }
 
     ConfigCatClient::closeAll();
 }
