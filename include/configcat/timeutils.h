e0daf4d35a673d3cc8b757085310a590385b68cb
kp-cat <52385411+kp-cat@users.noreply.github.com>
2024-04-02 09:22:14 +0200

    make_datetime millisec 0 default value

--- include/configcat/timeutils.h
+++ include/configcat/timeutils.h
@@ -17,6 +17,6 @@ namespace configcat {
 using date_time_t = std::chrono::system_clock::time_point;
 
 std::string datetime_to_isostring(const date_time_t& tp);
-date_time_t make_datetime(int year, int month, int day, int hour, int min, int sec, int millisec);
+date_time_t make_datetime(int year, int month, int day, int hour, int min, int sec, int millisec = 0);
 
 } // namespace configcat
